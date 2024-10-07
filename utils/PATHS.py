422f735ebc43efcc3bf4a8d3f543f356cfb136d1
mikedataCrunch <michaeldorosan11@gmail.com>
2024-10-07 15:26:27 +0800

    Updated paths to suit hsrc_michael ARC machine

--- utils/PATHS.py
+++ utils/PATHS.py
@@ -1,10 +1,16 @@
 import os
 
-DATA_ROOT = "/Users/mdorosan/Desktop/2024/datasets"
-DATA_DIR = os.path.join(DATA_ROOT, "elpha-fh")
+# DATA_ROOT = "/Users/mdorosan/Desktop/2024/datasets" # local_mac
+DATA_ROOT = "/mnt/d/PROJECT_ELPHA" # hsrc_michael
+DATA_DIR = os.path.join(DATA_ROOT, "FH/raw")
 
 LABS = os.path.join(DATA_DIR, "1 Lab General")
-DIAGNOSIS = os.path.join(DATA_DIR, "2 Diagnosis (with HtWtBMI)")
+DIAGNOSIS = os.path.join(DATA_DIR, "2 Diagnosis")
+DRUG_DISPENSED = os.path.join(DATA_DIR, "3 Drug Dispensed")
 PROBLEM_LIST = os.path.join(DATA_DIR, "4 Problem List")
-# DIAGNOSIS = os.path.join(DATA_DIR, "2 Diagnosis")
-DRUG_DISPENSED = os.path.join(DATA_DIR, "3 Drug_Dispensed_Subject")
+
+# LABS = os.path.join(DATA_DIR, "1 Lab General")
+# DIAGNOSIS = os.path.join(DATA_DIR, "2 Diagnosis (with HtWtBMI)")
+# PROBLEM_LIST = os.path.join(DATA_DIR, "4 Problem List")
+# # DIAGNOSIS = os.path.join(DATA_DIR, "2 Diagnosis")
+# DRUG_DISPENSED = os.path.join(DATA_DIR, "3 Drug_Dispensed_Subject")
