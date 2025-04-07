0b3c0e973f0c7e8725e19d1869fbbdaa37c26d8c
Mike Dorosan <64011162+mikedataCrunch@users.noreply.github.com>
2025-04-07 14:14:35 +0800

    Create temp.py

---
+++ utils/temp.py
@@ -0,0 +1,127 @@
+# temp file for CVD and CHD terms for personal history check
+cvd_terms = [
+    "Stroke",
+    "Cerebral Infarction",
+    "Hemorrhagic Stroke",
+    "Ischemic Stroke",
+    "Transient Ischemic Attack",
+    "Cerebral Hemorrhage",
+    "Subarachnoid Hemorrhage",
+    "Intracerebral Hemorrhage",
+    "Brain Aneurysm",
+    "Cerebral Aneurysm",
+    "Arteriovenous Malformation",
+    "Cerebral Venous Thrombosis",
+    "Cerebral Venous Sinus Thrombosis",
+    "Carotid Stenosis",
+    "Vertebrobasilar Insufficiency",
+    "Moyamoya Disease",
+    "Lacunar Stroke",
+    "Cerebral Arteriosclerosis",
+    "Carotid Artery Dissection",
+    "Cerebral Vasculitis",
+    "Cerebral Embolism",
+    "Vascular Dementia",
+    "Hypertensive Encephalopathy",
+    "Carotid Endarterectomy",
+    "Stroke Rehabilitation",
+    "Cerebrovascular Accident",
+    "Cerebrovascular Occlusion"
+]
+
+cvd_procedure_terms = [
+    "Coronary Artery Bypass Graft (CABG)",
+    "Percutaneous Coronary Intervention (PCI)",
+    "Angioplasty",
+    "Stent Placement",
+    "Cardiac Catheterization",
+    "Electrophysiological Study",
+    "Pacemaker Insertion",
+    "Implantable Cardioverter Defibrillator (ICD) Placement",
+    "Transcatheter Aortic Valve Replacement (TAVR)",
+    "Carotid Endarterectomy",
+    "Aneurysm Repair",
+    "Endovascular Repair of Aorta",
+    "Thrombectomy",
+    "Thrombolysis",
+    "Echocardiography",
+    "Cardiac MRI",
+    "Cardiac Stress Testing",
+    "Holter Monitoring",
+    "Electrocardiogram (ECG or EKG)",
+    "Loop Recorder Implantation",
+    "Pulmonary Thromboendarterectomy",
+    "Vascular Scaffolding",
+    "Cardiac Resynchronization Therapy (CRT)",
+    "Left Ventricular Assist Device (LVAD) Implantation",
+    "Heart Transplant"
+]
+
+
+chd_terms = [
+    "Atrial Septal Defect",
+    "Ventricular Septal Defect",
+    "Tetralogy of Fallot",
+    "Patent Ductus Arteriosus",
+    "Coarctation of the Aorta",
+    "Transposition of the Great Arteries",
+    "Hypoplastic Left Heart Syndrome",
+    "Pulmonary Atresia",
+    "Tricuspid Atresia",
+    "Total Anomalous Pulmonary Venous Return",
+    "Ebstein's Anomaly",
+    "Double Outlet Right Ventricle",
+    "Aortic Valve Stenosis",
+    "Pulmonary Valve Stenosis",
+    "Bicuspid Aortic Valve",
+    "Congenital Mitral Valve Anomalies",
+    "Single Ventricle Defects",
+    "Endocardial Cushion Defect",
+    "Mitral Valve Prolapse",
+    "Anomalous Pulmonary Venous Connection",
+    "Interrupted Aortic Arch",
+    "Atrioventricular Septal Defect",
+    "Congenital Aortic Valve Insufficiency",
+    "Congenital Pulmonary Valve Insufficiency",
+    "Right Hypoplastic Heart Syndrome",
+    "Left Hypoplastic Heart Syndrome",
+    "Congenital Tricuspid Valve Stenosis",
+    "Congenital Tricuspid Valve Insufficiency",
+    "Subaortic Stenosis",
+    "Supravalvular Aortic Stenosis",
+    "Cor Triatriatum",
+    "Pulmonary Artery Stenosis",
+    "Pulmonary Artery Atresia",
+    "Anomalous Left Coronary Artery From the Pulmonary Artery",
+    "Anomalous Right Coronary Artery From the Pulmonary Artery",
+    "Coronary Artery Anomaly",
+    "Dextrocardia",
+    "Eisenmenger Syndrome",
+    "Mitral Atresia",
+    "Double Inlet Left Ventricle",
+    "Double Inlet Right Ventricle",
+    "Uhl's Anomaly",
+    "Heterotaxy Syndrome",
+    "Congenital Heart Block",
+    "L-transposition of the great arteries",
+    "D-transposition of the great arteries",
+    "Shone's Complex",
+    "Heart Malformation",
+    "Heart Defects, Congenital",
+    "Congenital Cardiovascular Anomaly"
+]
+
+chd_procedure_terms = [
+    "Heart Valve Replacement",
+    "Heart Valve Repair",
+    "Atrial or Ventricular Septal Defect Repair",
+    "Transcatheter Aortic Valve Replacement (TAVR)",  # Also used in CVD
+    "Echocardiography",  # Also used in CVD
+    "Cardiac Catheterization",  # Also used in CVD
+    "Electrophysiological Study",  # Also used in CVD
+    "Pacemaker Insertion",  # Also used in CVD for arrhythmias that can be congenital
+    "Implantable Cardioverter Defibrillator (ICD) Placement",  # Also used in CVD
+    "Cardiac MRI",  # Also used in CVD
+    "Heart Transplant"  # Also used in severe CHD cases
+]
+
