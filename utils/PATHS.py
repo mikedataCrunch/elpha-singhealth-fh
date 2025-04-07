0da839ecd8512aa016ef5c3c67035b98704bf810
mikedataCrunch <michaeldorosan11@gmail.com>
2025-04-07 17:00:31 +0800

    Fixed loading of paths from env var

--- utils/PATHS.py
+++ utils/PATHS.py
@@ -1,18 +1,13 @@
-import os
+from dotenv import load_dotenv
+import os, sys
 
-DATA_ROOT = "/Users/mdorosan/Desktop/2024/datasets" # local_mac
-DATA_DIR = os.path.join(DATA_ROOT, "elpha-fh")
+from dotenv import load_dotenv
+load_dotenv("../.env")  # take environment variables
+PROJECT_ROOT = os.environ.get("PROJECT_ROOT")
 
-# DATA_ROOT = "/mnt/d/PROJECT_ELPHA" # hsrc_michael
-# DATA_DIR = os.path.join(DATA_ROOT, "FH/raw")
+DATA_ROOT = os.path.join(PROJECT_ROOT, "data", "processed")
 
-# LABS = os.path.join(DATA_DIR, "1 Lab General")
-# DIAGNOSIS = os.path.join(DATA_DIR, "2 Diagnosis")
-# DRUG_DISPENSED = os.path.join(DATA_DIR, "3 Drug Dispensed")
-# PROBLEM_LIST = os.path.join(DATA_DIR, "4 Problem List")
-
-LABS = os.path.join(DATA_DIR, "1 Lab General")
-DIAGNOSIS = os.path.join(DATA_DIR, "2 Diagnosis (with HtWtBMI)")
-PROBLEM_LIST = os.path.join(DATA_DIR, "4 Problem List")
-# DIAGNOSIS = os.path.join(DATA_DIR, "2 Diagnosis")
-DRUG_DISPENSED = os.path.join(DATA_DIR, "3 Drug_Dispensed_Subject")
+LABS = os.path.join(DATA_ROOT, "1 Lab General")
+DIAGNOSIS = os.path.join(DATA_ROOT, "2 Diagnosis")
+DRUG_DISPENSED = os.path.join(DATA_ROOT, "3 Drug Dispensed")
+PROBLEM_LIST = os.path.join(DATA_ROOT, "4 Problem List")
