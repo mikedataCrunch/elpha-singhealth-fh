76effa691c2120dec068928c06b830945d57a7b0
Mike Dorosan <64011162+mikedataCrunch@users.noreply.github.com>
2025-04-08 15:55:43 +0800

    Create physical_exam_terms.py

---
+++ utils/physical_exam_terms.py
@@ -0,0 +1,50 @@
+physical_exam_terms = [
+  # Xanthomas
+  Achilles Tendinous Xanthomas  
+  Achilles tendon xanthoma  
+  Cutaneous Xanthomas  
+  Nodular Achilles Tendons  
+  Palmar Xanthomas  
+  Palpable Xanthomas  
+  Possible Tendon Xanthoma 
+  Tendinous Xanthoma  
+  Tendinous Xanthomas  
+  Tendinous Xanthomata  
+  Tendon Xanthoma  
+  Tendon Xanthomas  
+  Tendon Xanthomata  
+  Thickened Achilles Tendon  
+  Thickening Of Achilles Tendon  
+  Tuberoeruptive Xanthomas  
+  Tuberous Xanthomas  
+  Xanthomas  
+  Xanthomas Of Achilles Tendon 
+  Xanthomas Of The Hands 
+  Xanthomas On The Extensor Surfaces 
+
+# corneal arcus
+Corneal arcus 
+Arcus cornealis
+Early arcus senilis 
+
+]
+
+exclude = [
+Eruptive xanthomas 
+Irruptive xanthomas  
+Xanthomas around both eyes  
+Xanthomas around the left eye  
+Xanthomas around the right eye  
+Eyelid xanthomas  
+Eye lid xanthomas  
+Xanthomas on the eye  
+Xanthomas over eyelids  
+Xanthomas over the eyes  
+Xanthomas over both eyelids  
+Xanthomas on the upper lids  
+Xanthomas beneath both eyes  
+Xanthomas beneath eye(s)  
+Xanthomas beneath eyelid 
+Xanthomas on the face 
+  
+]
