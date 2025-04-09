9c9e1b0255da357914ebc2e775b6338059da35a8
mikedataCrunch <michaeldorosan11@gmail.com>
2025-04-09 10:12:27 +0800

    key terms

--- utils/physical_exam_terms.py
+++ utils/physical_exam_terms.py
@@ -1,50 +1,48 @@
 physical_exam_terms = [
-  # Xanthomas
-  Achilles Tendinous Xanthomas  
-  Achilles tendon xanthoma  
-  Cutaneous Xanthomas  
-  Nodular Achilles Tendons  
-  Palmar Xanthomas  
-  Palpable Xanthomas  
-  Possible Tendon Xanthoma 
-  Tendinous Xanthoma  
-  Tendinous Xanthomas  
-  Tendinous Xanthomata  
-  Tendon Xanthoma  
-  Tendon Xanthomas  
-  Tendon Xanthomata  
-  Thickened Achilles Tendon  
-  Thickening Of Achilles Tendon  
-  Tuberoeruptive Xanthomas  
-  Tuberous Xanthomas  
-  Xanthomas  
-  Xanthomas Of Achilles Tendon 
-  Xanthomas Of The Hands 
-  Xanthomas On The Extensor Surfaces 
-
-# corneal arcus
-Corneal arcus 
-Arcus cornealis
-Early arcus senilis 
-
+    # Xanthomas
+    "Achilles Tendinous Xanthomas",
+    "Achilles tendon xanthoma",
+    "Cutaneous Xanthomas",
+    "Nodular Achilles Tendons",
+    "Palmar Xanthomas",
+    "Palpable Xanthomas",
+    "Possible Tendon Xanthom",
+    "Tendinous Xanthoma",
+    "Tendinous Xanthomas",
+    "Tendinous Xanthomata",
+    "Tendon Xanthoma",
+    "Tendon Xanthomas",
+    "Tendon Xanthomata",
+    "Thickened Achilles Tendon",
+    "Thickening Of Achilles Tendon",
+    "Tuberoeruptive Xanthomas",
+    "Tuberous Xanthomas",
+    "Xanthomas",
+    "Xanthomas Of Achilles Tendon",
+    "Xanthomas Of The Hands",
+    "Xanthomas On The Extensor Surfaces",
+    
+    # corneal arcus
+    "Corneal arcus",
+    "Arcus cornealis",
+    "Early arcus senilis",
 ]
 
-exclude = [
-Eruptive xanthomas 
-Irruptive xanthomas  
-Xanthomas around both eyes  
-Xanthomas around the left eye  
-Xanthomas around the right eye  
-Eyelid xanthomas  
-Eye lid xanthomas  
-Xanthomas on the eye  
-Xanthomas over eyelids  
-Xanthomas over the eyes  
-Xanthomas over both eyelids  
-Xanthomas on the upper lids  
-Xanthomas beneath both eyes  
-Xanthomas beneath eye(s)  
-Xanthomas beneath eyelid 
-Xanthomas on the face 
-  
+physical_exam_exclude_terms = [
+    "Eruptive xanthomas",
+    "Irruptive xanthomas",
+    "Xanthomas around both eyes",
+    "Xanthomas around the left eye",
+    "Xanthomas around the right eye",
+    "Eyelid xanthomas",
+    "Eye lid xanthomas",
+    "Xanthomas on the eye",
+    "Xanthomas over eyelids",
+    "Xanthomas over the eyes",
+    "Xanthomas over both eyelids",
+    "Xanthomas on the upper lids",
+    "Xanthomas beneath both eyes",
+    "Xanthomas beneath eye(s)",
+    "Xanthomas beneath eyelid",
+    "Xanthomas on the face",
 ]
--- utils/utils.py
+++ utils/utils.py
@@ -285,20 +285,55 @@ chd_procedure_terms = [
     "Heart Transplant"  # Also used in severe CHD cases
 ]
 
-# stage 2 utils
-personal_history_snomed = [
-        
-]
-personal_history_icd10 = [
-    
+physical_exam_xa_terms = [
+    # Xanthomas
+    "Achilles Tendinous Xanthomas",
+    "Achilles tendon xanthoma",
+    "Cutaneous Xanthomas",
+    "Nodular Achilles Tendons",
+    "Palmar Xanthomas",
+    "Palpable Xanthomas",
+    "Possible Tendon Xanthom",
+    "Tendinous Xanthoma",
+    "Tendinous Xanthomas",
+    "Tendinous Xanthomata",
+    "Tendon Xanthoma",
+    "Tendon Xanthomas",
+    "Tendon Xanthomata",
+    "Thickened Achilles Tendon",
+    "Thickening Of Achilles Tendon",
+    "Tuberoeruptive Xanthomas",
+    "Tuberous Xanthomas",
+    "Xanthomas",
+    "Xanthomas Of Achilles Tendon",
+    "Xanthomas Of The Hands",
+    "Xanthomas On The Extensor Surfaces",
 ]
 
-physical_exam_snomed = [
-    
+physical_exam_ca_terms = [
+    # corneal arcus
+    "Corneal arcus",
+    "Arcus cornealis",
+    "Early arcus senilis",
 ]
 
-physical_exam_icd10 = [
-    
+physical_exam_exclude_terms = [
+    "Eruptive xanthomas",
+    "Irruptive xanthomas",
+    "Xanthomas around both eyes",
+    "Xanthomas around the left eye",
+    "Xanthomas around the right eye",
+    "Eyelid xanthomas",
+    "Eye lid xanthomas",
+    "Xanthomas on the eye",
+    "Xanthomas over eyelids",
+    "Xanthomas over the eyes",
+    "Xanthomas over both eyelids",
+    "Xanthomas on the upper lids",
+    "Xanthomas beneath both eyes",
+    "Xanthomas beneath eye(s)",
+    "Xanthomas beneath eyelid",
+    "Xanthomas on the face",
 ]
 
 # # stage 1 utils
